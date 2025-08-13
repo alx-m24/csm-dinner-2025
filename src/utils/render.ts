b4c4f0e659731c89cf7f0abb9950c68e12bec549
Alexandre Mohamudally <alexandremohamudally@gmail.com>
2025-08-13 19:13:34 +0400

    Test

--- src/utils/render.ts
+++ src/utils/render.ts
@@ -1,4 +1,19 @@
-export async function renderHtml(fileName: string) {
-    const file = await import(`../pages/${fileName}?raw`);
-    return file.default;
+﻿export async function renderHtml(fileName: string) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>D1</title>
+        <link rel="stylesheet" type="text/css" href="https://static.integrations.cloudflare.com/styles.css">
+      </head>
+    
+      <body>
+        <main>
+          <p>Your D1 Database contains the following data:</p>
+        </main>
+      </body>
+    </html>
+`;
 }
